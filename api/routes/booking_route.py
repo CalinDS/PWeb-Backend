fe3b23c12be213d733ec9bf65105bdef86a1b247
Lucian Roinita <roinitalucian@yahoo.com>
2022-05-21 17:31:35 +0300

    added volunteer_bookings get endpoint

--- api/routes/booking_route.py
+++ api/routes/booking_route.py
@@ -1,8 +1,10 @@
 from distutils.log import error
 import json
-from flask import request, Blueprint
+from flask import request, Blueprint, jsonify
 from infrastrucure.db_config import db
 from core.model.booking_model import BookingModel
+from core.model.accommodation_model import AccommodationModel
+from core.model.user_model import UserModel
 
 bookings_api = Blueprint('bookings_api', __name__)
 
@@ -29,6 +31,34 @@ def create_booking():
 def retrieve_bookings():
     bookings = BookingModel.query.all()
     return str(bookings), 200
+
+#retrieve
+@bookings_api.route('/bookings/<int:id>', methods = ['GET'])
+def retrieve_volunteer_bookings(id):
+    accommodations = AccommodationModel.query.filter_by(owner_id=id).all()
+    bookings_by_accommodation = []
+    for a in accommodations:
+        bookings = BookingModel.query.filter_by(accommodation_id=a.id).all()
+        if len(bookings) == 0:
+            continue
+        refugees = []
+        for b in bookings:
+            ref = UserModel.query.filter_by(id=b.refugee_id).first()
+            refugees.append({
+                "refugee_name": ref.name,
+                "family_members_no": ref.family_members_no,
+                "contact_info": ref.contact_info
+            })
+        bookings_by_accommodation.append({
+            "accommodation":  {
+                "address": a.address,
+                "photo": a.photo,
+                "beds_no": a.beds_no
+            },
+            "refugees": refugees
+        })
+
+    return jsonify(bookings_by_accommodation), 200
 
 
 #update
